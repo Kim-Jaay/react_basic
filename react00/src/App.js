7b9217b4422bedbdaf1ed60a5eb6a5a743717cc8
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-07-25 12:08:22 +0900

    0725-02

---
+++ react00/src/App.js
@@ -0,0 +1,19 @@
+import Test from "./test";
+import Nav from "./Nav";
+
+const App = () => {
+  // console.log('헤헤')
+  return (
+    <>
+      <Nav />
+      <Test />
+      <Test />
+      <Test />
+      <Test />
+      <h1>히히</h1>
+      {/* <h2>헤헤헤헨</h2> */}
+    </>
+  )
+}
+
+export default App;
---
+++ react00/src/test.js
@@ -0,0 +1,10 @@
+const Test = () => {
+    return (
+        <>
+            <i className="xi-bars"></i>
+            <p>Lorem ipsum dolor sit.</p>
+        </>
+    )
+}
+
+export default Test;
